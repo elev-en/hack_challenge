c2a38ba13c11c9f4081abc1c11eb9f7faf9c5f72
ect65 <ect65@cornell.edu>
2022-11-15 22:24:18 -0500

    Initial Commit

---
+++ hack_challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hack_challenge
+//
+//  Created by Elaine Tsai on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
