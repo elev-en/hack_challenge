33a80bc36a08a36ee8d171ba4f2e782322c5f307
ect65 <ect65@cornell.edu>
2022-11-22 00:03:50 -0500

    worked on create account and log in view controllers

--- hack_challenge/CreateUsernamePasswordViewController.swift
+++ hack_challenge/CreateUsernamePasswordViewController.swift
@@ -9,10 +9,52 @@ import UIKit
 
 class CreateUsernamePasswordViewController: UIViewController {
     
+    let background = UIImageView()
+    let welcomeLabel = UILabel()
+    let usernameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let confirmPasswordTextField = UITextField()
+    let usernameStackView = UIStackView()
+    let passwordStackView = UIStackView()
+    let confirmPasswordStackView = UIStackView()
    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        background.contentMode = .scaleAspectFill
+        background.image = UIImage(named: "background")
+        background.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(background)
+        
+        let ovalPath = UIBezierPath(ovalIn: CGRect(x: view.bounds.width/10-view.bounds.width/5, y: view.bounds.height/10, width: view.bounds.width, height: view.bounds.height*0.25))
+
+        let fillLayer = CAShapeLayer()
+        fillLayer.path = ovalPath.cgPath
+        fillLayer.fillColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).cgColor
+        fillLayer.opacity = 0.5
+        view.layer.addSublayer(fillLayer)
+        
+        welcomeLabel.text = "Welcome to CUStudy!"
+        welcomeLabel.font = .systemFont(ofSize: 25)
+        welcomeLabel.adjustsFontForContentSizeCategory = true
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(welcomeLabel)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints(){
+        NSLayoutConstraint.activate([
+            background.topAnchor.constraint(equalTo: view.topAnchor),
+            background.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            background.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -view.bounds.width/10),
+            welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: view.bounds.height*0.1)
+        ])
     }
 
 }
--- hack_challenge/LogInUsernamePasswordViewController.swift
+++ hack_challenge/LogInUsernamePasswordViewController.swift
@@ -9,21 +9,50 @@ import UIKit
 
 class LogInUsernamePasswordViewController: UIViewController {
 
+    let background = UIImageView()
+    let welcomeBackLabel = UILabel()
+    let usernameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let usernameStackView = UIStackView()
+    let passwordStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        background.contentMode = .scaleAspectFill
+        background.image = UIImage(named: "background")
+        background.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(background)
+        
+        let ovalPath = UIBezierPath(ovalIn: CGRect(x: view.bounds.width/10-view.bounds.width/5, y: view.bounds.height/10, width: view.bounds.width, height: view.bounds.height*0.25))
+
+        let fillLayer = CAShapeLayer()
+        fillLayer.path = ovalPath.cgPath
+        fillLayer.fillColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).cgColor
+        fillLayer.opacity = 0.5
+        view.layer.addSublayer(fillLayer)
+        
+        welcomeBackLabel.text = "Welcome back to \nCUStudy!"
+        welcomeBackLabel.numberOfLines = 2
+        welcomeBackLabel.font = .systemFont(ofSize: 25)
+        welcomeBackLabel.adjustsFontForContentSizeCategory = true
+        welcomeBackLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(welcomeBackLabel)
+        
+        setupConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupConstraints(){
+        NSLayoutConstraint.activate([
+            background.topAnchor.constraint(equalTo: view.topAnchor),
+            background.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            background.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            welcomeBackLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -view.bounds.width/10),
+            welcomeBackLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: view.bounds.height*0.08)
+        ])
     }
-    */
 
 }
